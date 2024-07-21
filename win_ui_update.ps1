6609ba3073cad008db2d02c036bab6d182c4f63c
Ayaal Ali <73424845+ayol377@users.noreply.github.com>
2024-07-21 14:01:14 +0500

    Update win_ui_update.ps1

--- win_ui_update.ps1
+++ win_ui_update.ps1
@@ -1,1 +1,1 @@
-do{rundll32.exe user32.dll, UpdatePerUserSystemParameters, 1, True; Start-Sleep -Milliseconds 1}while($a++-le100)
+$a=1;do{rundll32.exe user32.dll, UpdatePerUserSystemParameters, 1, True; Start-Sleep -Milliseconds 1}while($a++-le100)
